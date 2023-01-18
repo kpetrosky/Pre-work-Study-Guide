7adeeba57cd9e099be4a65b850bc4f55911c5972
kpetrosky <kpetrosky36@gmail.com>
2023-01-17 19:11:23 -0700

    updated 10

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,13 +1,5 @@
-var topic = "HTML";
+var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics);
 
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
